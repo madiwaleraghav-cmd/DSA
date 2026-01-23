11ec5d9e18e971643ed7e0114a5f61ebcd8c2f61
Raghavendra <madiwaleraghav@gmail.com>
2026-01-23 14:08:23 +0530

    Update Choclate.java

--- Choclate.java
+++ Choclate.java
@@ -1,34 +1,29 @@
-class Main {
-    public static void main(String[] args) {
-        int[] a={12,41,23,53,32};
-        // 12 23 32 41 53 
-        int temp=0,m=3;
-        int diff=0,j,fmin=0,mindiff=Integer.MAX_VALUE;
-        for(int i=0;i<a.length;i++){
-            for( j=0;j<a.length;j++){
-                if(a[i]<a[j]){
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-                }
-            }
-        }
-        for(int i=0;i<a.length;i++){
-          for(j=i;j<m;j++){
-              System.out.print(a[j]+" ");
-           diff=a[j]-a[i];
-           
-             
-          }
-         mindiff=Math.min(mindiff,diff);
-          m+=1;
-          if(j==a.length){
-              break;
-          }
-          System.out.println();
-         
-        }
-        System.out.print(mindiff);
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class choclate {
+    public static int min(int[] nums,int m){
+        int dif=0;
+        int a,b;
+        ArrayList<Integer> a1=new ArrayList<>();
+        for(int x:nums){
+            a1.add(x);
+        }
+        a1.sort(null);
+        for(int i=0;i<m;i++){
+            a=a1.get(0);
+            b=a1.get(m-1);
+            dif=b-a;
+        } 
+        return dif;
+    }
+
+   
+    public static void main(String[] args) {
+        int[] nums={7, 3, 2, 4, 9, 12, 56};
+       int result=choclate.min(nums,5);
+       System.out.print(result);
+        
     }
 }
